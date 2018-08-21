ecfbf73a6f8d58a976486d24d6ff38c38fe697a8
Ethan Hampton <ethohampton@gmail.com>
2018-08-21 10:19:25 -0700

    Remove close icon from climate popup

--- dev/js/climate.js
+++ dev/js/climate.js
@@ -13,12 +13,12 @@ function setupClimate() {
     }
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    //var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        //modal.style.display = "none";//Currently not using this as we want the user to answer the question
-    };
+    //span.onclick = function () {
+    //modal.style.display = "none";//Currently not using this as we want the user to answer the question
+    //};
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
