849c732bfc1d937526a8498fd41549d2c16f60b6
Ethan Hampton <ethohampton@gmail.com>
2020-05-11 11:49:49 -0700

    Reduce code complexity via shortcut from Callib

--- dev/js/calendar.js
+++ dev/js/calendar.js
@@ -43,29 +43,9 @@ function getDayFirstDate(d) {
 }
 
 function howManyDays(d) {
-    var m = d.getMonth() + 1;
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) return 31;
-    if (m == 2) {
-        if (isLeapYear(d.getFullYear())) {
-            return 29;
-        } else {
-            return 28;
-        }
-    }
-    return 30;
+    return new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
 }
 
-function isLeapYear(year) {
-    if (year % 400 == 0) {
-        return true;
-    } else if (year % 100 == 0) {
-        return false;
-    } else if (year % 4 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
 /**
  *
  * @param {Date} d the date
