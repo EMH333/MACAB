6e25aac1c75b12d6a7f5bb94eca16c46b19fe953
Ethan Hampton <ethohampton@gmail.com>
2021-09-12 15:38:56 -0700

    Update version

--- dev/serviceWorker/sw.js
+++ dev/serviceWorker/sw.js
@@ -1,4 +1,4 @@
-let CACHE_NAME = 'abday-ethohampton-cache-v2.0.2';
+let CACHE_NAME = 'abday-ethohampton-cache-v2.1.0';
 let cacheWhitelist = [CACHE_NAME]; //NOTE If we change files, change cache name so we update them
 let urlsToCache = [
     '/',
